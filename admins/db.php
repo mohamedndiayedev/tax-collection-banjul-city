f34785a843b3dbb22159431b15efda1fd7cdc293
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-25 13:26:37 -1200

    Update db.php

--- admins/db.php
+++ admins/db.php
@@ -1,3 +1,3 @@
 <?php
-	$con=mysqli_connect("localhost","root","","muthurwa");
+	$con=mysqli_connect("qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","rw8byqbq83ydbxyx","w00tj6270z0srv8b","bn3044t3148uwved");
 ?>
